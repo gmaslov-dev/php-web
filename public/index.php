572ee2f3b74d64447a8770a094155b121e06898d
gmaslov-dev <gennadiy.dev@yandex.ru>
2025-07-14 19:52:16 +0300

    add basic routes

--- public/index.php
+++ public/index.php
@@ -1,4 +1,21 @@
 <?php
 
-$uri = $_SERVER['REQUEST_URI'];
+$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+switch ($path) {
+  case '/':
+    echo <<<EOT
+    <a href="/welcome">welcome</a>
+    <br>
+    <a href="/not-found">not-found</a>
+    EOT;
+    break;
+  case '/welcome':
+    echo '<a href="/">main</a>';
+    break;
+  default:
+    header("HTTP/1.1 404");
+    echo 'Page not found. <a href="/">main</a>';
+}
+
 
