ac8a7c3a721b6b6cbb3438fcc6232bb44fa7c57c
gmaslov-dev <gennadiy.dev@yandex.ru>
2025-07-20 19:09:09 +0300

    add templates

--- public/index.php
+++ public/index.php
@@ -1,26 +1,43 @@
 <?php
 
         // Подключение автозагрузки через composer
+use Slim\Factory\AppFactory;
+use DI\Container;
+use User\PhpWeb\Generator;
+
 require __DIR__ . '/../vendor/autoload.php';
 
-use Slim\Factory\AppFactory;
+// Список пользователей
+// Каждый пользователь – ассоциативный массив
+// следующей структуры: id, firstName, lastName, email
+$users = Generator::generate(100);
 
+$container = new Container();
+$container->set('renderer', function () {
+    return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
+});
+
+AppFactory::setContainer($container);
 $app = AppFactory::create();
 $app->addErrorMiddleware(true, true, true);
 
-$companies = User\PhpWeb\Generator::generate(100);
-
 $app->get('/', function ($request, $response) {
-    return $response->write('go to the /companies');
+    return $this->get('renderer')->render($response, 'index.phtml');
 });
 
-// BEGIN (write your solution here)
-$app->get('/companies', function($request, $response) use ($companies) {
-    $page = $request->getQueryParam('page', 1);
-    $per = $request->getQueryParam('per', 5);
 
-    $result = array_slice($companies, ($page - 1) * $per, $per);
-    return $response->withJson($result);
+
+
+$app->get('/users', function ($request, $response) use ($users) {
+    return $this->get('renderer')->render($response, 'users/index.phtml', ['users' => $users]);
+});
+
+$app->get('/users/{id}', function ($request, $response, $args) use ($users) {
+    if (isset($users[$args['id']])) {
+        return $this->get('renderer')->render($response, 'users/show.phtml', ['user' => $users[$args['id']]]);
+    } else {
+        return $response->withStatus(404)->write('User not found');
+    }
 });
 // END
 
