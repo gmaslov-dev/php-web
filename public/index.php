946e15ab3c239a81acb8bfab46fba839d068cf6f
gmaslov-dev <gennadiy.dev@yandex.ru>
2025-08-24 20:46:22 +0300

    refactor routers

--- public/index.php
+++ public/index.php
@@ -5,6 +5,7 @@ use PhpWeb\UserDAO;
 use PhpWeb\UserValidator;
 use Slim\Factory\AppFactory;
 use DI\Container;
+use Slim\Routing\RouteContext;
 use Slim\Views\PhpRenderer;
 
 require __DIR__ . '/../vendor/autoload.php';
@@ -14,82 +15,80 @@ session_start();
 $conn = new PDO('sqlite:../php-web.sqlite');
 $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
-$dao = new UserDAO($conn);
-
 $container = new Container();
+// Регистрируем DAO
+$container->set(UserDAO::class, function () use ($conn) {
+    return new UserDAO($conn);
+});
+
+// Регистрируем Renderer
 $container->set('renderer', function () {
     return new PhpRenderer(__DIR__ . '/../templates');
 });
 
+// Регистрируем Flash
 $container->set('flash', function () {
     return new \Slim\Flash\Messages();
 });
 
 AppFactory::setContainer($container);
 $app = AppFactory::create();
+
+// Мидлвары
 $app->addRoutingMiddleware();     // Сначала роутинг
 $app->addBodyParsingMiddleware(); // Потом парсинг тела
 $app->addErrorMiddleware(true, true, true); // И ошибки в конце
 
-$router = $app->getRouteCollector()->getRouteParser();
+// Маршруты
+$app->get('/', function ($request, $response) use ($app) {
+    $renderer = $app->getContainer()->get(PhpRenderer::class);
 
-// стартовая страница
-$app->get('/', function ($request, $response) use ($router){
-
-    $renderer = $this->get('renderer')->fetch('index.phtml');
 
     $params = [
         'title' => 'Homepage',
-        'content' => $renderer,
+        'header' => $renderer->fetch('../templates/nav.phtml'),
         'router' => $router
     ];
-    return $this->get('renderer')->render($response, "layout.phtml", $params);
+    return $renderer->render($response, "index.phtml", $params);
 })->setName('home');
 
 
-// пользователи
-$app->get('/users', function ($request, $response) use ($dao, $app, $router) {
-    $users = $dao->getAll();
-    return $this->get('renderer')->render($response, "users/index.phtml", ['users' => $users, 'router' => $router]);
-})->setName('users.get');
-
-
-
-
-
-
-
-$app->post('/users', function ($request, $response) use ($dao, $app, $router) {
-    $validator = new UserValidator();
-    $user = $request->getParsedBodyParam('user');
-    $errors = $validator->validate($user, $dao);
-    $this->get('flash')->addMessage('success', 'User was added succesfully');
-
-    if (count($errors) === 0) {
-        $user = new User($user['nickname'], $user['email']);
-        $dao->save($user);
-        return $response->withRedirect('/users', 302);
-    }
-    return $response->withRedirect('/users');
-})->setName('users.post');
-
-
-
-$app->get('/users/new', function ($request, $response) use ($router) {
-    $renderer = $this->get('renderer')->fetch('users/new.phtml', ['router' => $router]);
-    return $this->get('renderer')->render($response, "layout.phtml", ['content' => $renderer, 'router' => $router]);
-})->setName('users/new');
-
-$app->get('/users/{id}', function ($request, $response) use ($dao, $router) {
-    $userId = $request->getAttribute('id');
-    $user = $dao->find($userId);
-
-    if (!$user) {
-        return $response->withStatus(404)->write('Пользователь не найден');
-    }
-
-    $renderer = $this->get('renderer')->fetch('users/show.phtml', ['name' => $user->getName(), 'email' => $user->getEmail()]);
-
-    return $this->get('renderer')->render($response, "layout.phtml", ['content' => $renderer, 'router' => $router]);
-});
+//// пользователи
+//$app->get('/users', function ($request, $response) {
+//    $dao = $this->get(UserDAO::class);
+//    $renderer = $this->get(PhpRenderer::class);
+//    $router = $this->getRouter()->getRouteParser();
+//
+//    $users = $dao->getAll();
+//    return $this->get('renderer')->render($response, "users/index.phtml", ['users' => $users, 'router' => $router, 'renderer' => $renderer]);
+//})->setName('users.get');
+//
+//$app->post('/users', function ($request, $response) {
+//    $validator = new UserValidator();
+//    $user = $request->getParsedBodyParam('user');
+//    $errors = $validator->validate($user, $dao);
+//    $this->get('flash')->addMessage('success', 'User was added succesfully');
+//
+//    if (count($errors) === 0) {
+//        $user = new User($user['nickname'], $user['email']);
+//        $dao->save($user);
+//        return $response->withRedirect('/users', 302);
+//    }
+//    return $response->withRedirect('/users');
+//})->setName('users.post');
+//
+//
+//
+//$app->get('/users/new', function ($request, $response)  {
+//    $renderer = $this->get('renderer')->fetch('users/new.phtml');
+//    return $this->get('renderer')->render($response, "layout.phtml", ['content' => $renderer, 'router' => $router]);
+//})->setName('users/new');
+//
+//$app->get('/users/{id}', function ($request, $response) {
+//    $userId = $request->getAttribute('id');
+//
+//
+//
+//    return $this->get('renderer')->render($response, "layout.phtml");
+//});
 $app->run();
