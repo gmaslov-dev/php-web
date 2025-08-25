3419a58272f3aef4d3b392a7317710ab9250931e
gmaslov-dev <gennadiy.dev@yandex.ru>
2025-08-25 16:01:29 +0300

    add pages

--- public/index.php
+++ public/index.php
@@ -5,6 +5,7 @@ use PhpWeb\UserDAO;
 use PhpWeb\UserValidator;
 use Slim\Factory\AppFactory;
 use DI\Container;
+use Slim\Flash\Messages;
 use Slim\Routing\RouteContext;
 use Slim\Views\PhpRenderer;
 
@@ -28,40 +29,54 @@ $container->set('renderer', function () {
 
 // Регистрируем Flash
 $container->set('flash', function () {
-    return new \Slim\Flash\Messages();
+    return new Messages();
 });
 
-AppFactory::setContainer($container);
-$app = AppFactory::create();
-
-// Мидлвары
-$app->addRoutingMiddleware();     // Сначала роутинг
-$app->addBodyParsingMiddleware(); // Потом парсинг тела
-$app->addErrorMiddleware(true, true, true); // И ошибки в конце
+$app = AppFactory::createFromContainer($container);
+$app->addErrorMiddleware(true, true, true);
 
 // Маршруты
-$app->get('/', function ($request, $response) use ($app) {
-    $renderer = $app->getContainer()->get(PhpRenderer::class);
-
-
-    $params = [
-        'title' => 'Homepage',
-        'header' => $renderer->fetch('../templates/nav.phtml'),
-        'router' => $router
-    ];
-    return $renderer->render($response, "index.phtml", $params);
+$app->get('/', function ($request, $response) {
+    return $this->get('renderer')->render($response, 'index.phtml');
 })->setName('home');
 
 
-//// пользователи
-//$app->get('/users', function ($request, $response) {
-//    $dao = $this->get(UserDAO::class);
-//    $renderer = $this->get(PhpRenderer::class);
-//    $router = $this->getRouter()->getRouteParser();
-//
-//    $users = $dao->getAll();
-//    return $this->get('renderer')->render($response, "users/index.phtml", ['users' => $users, 'router' => $router, 'renderer' => $renderer]);
-//})->setName('users.get');
+// Пользователи
+$app->get('/users', function ($request, $response) {
+    $limit = 5;
+    $page = (int) $request->getQueryParam('page', 1);
+    $currentPage = max(1, $page);
+
+    $users = $this->get(UserDAO::class)->getAll();
+    $totalPages = ceil(count($users) / $limit);
+    $currentUsers = array_slice($users, (($page - 1) * $limit), $limit);
+
+    $params = [
+        'users' => $currentUsers,
+        'page' => $currentPage,
+        'pages' => $totalPages
+    ];
+
+    return $this->get('renderer')->render($response, "users/index.phtml", $params);
+})->setName('users');
+
+
+
+// Пользователь
+$app->get('/users/{id}', function ($request, $response, array $args) {
+    $id = $args['id'];
+    $UserDAO = $this->get(UserDAO::class);
+    $user = $UserDAO->find($id);
+
+    if (!$user) {
+       return $response->write('Page not found')->withStatus(404);
+    }
+
+    $params = ['user' => $user];
+
+    return $this->get('renderer')->render($response, 'users/show.phtml', $params);
+})->setName('user');
+
 //
 //$app->post('/users', function ($request, $response) {
 //    $validator = new UserValidator();
