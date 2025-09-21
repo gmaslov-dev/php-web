cacb43797f7032cc692621fa342cc09fc0c7f484
gmaslov-dev <gennadiy-dev@yandex.ru>
2025-09-21 19:54:14 +0300

    add log auth middlware

--- src/Middleware/AuthMiddleware.php
+++ src/Middleware/AuthMiddleware.php
@@ -1,1 +1,28 @@
 <?php
+
+
+namespace PhpWeb\Middleware;
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
+use Slim\Psr7\Response as SlimResponse;
+
+class AuthMiddleware
+{
+    public function __invoke(Request $request, RequestHandler $handler): Response
+    {
+        // Проверяем авторизацию по сессии
+        if (!isset($_SESSION['user'])) {
+            $response = new SlimResponse();
+            $response->getBody()->write('Unauthorized. Please login.');
+            return $response->withStatus(401);
+            // или редирект:
+            // return $response
+            //     ->withHeader('Location', '/login')
+            //     ->withStatus(302);
+        }
+
+        return $handler->handle($request);
+    }
+}
